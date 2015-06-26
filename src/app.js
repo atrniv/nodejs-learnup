0a0cb6873cf0c2fbbc1c7ade00ee284bf74f4d7f
Johny Jose <johny@playlyfe.com>
2015-06-27 01:49:29 +0530

    feat(core): add jade templates
    Signed-off-by: Johny Jose <johny@playlyfe.com>

--- src/app.js
+++ src/app.js
@@ -4,25 +4,15 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var app = express();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
 app.use(cookieParser());
 app.use(session({ resave: false, saveUninitialized: true, secret: 'foo' }));
 app.use(bodyParser.json());
 
-app.use(function (req, res, next) {
-  if (req.session.requests == null) {
-    req.session.requests = 0;
-  } else {
-    req.session.requests++;
-  }
-  next();
-});
-
 app.get('/', function (req, res) {
-  res.json({ ok: true, requests: req.session.requests });
+  res.render('home');
 });
-
-app.post('/', function (req, res) {
-  res.json(req.body);
-})
 
 app.listen(3000);
