65d14b7bc7ab5ecf4c9aa33a1dc5265c681ce45c
Johny Jose <johny@playlyfe.com>
2015-06-26 23:38:54 +0530

    feat(core): add basic session and middleware
    Signed-off-by: Johny Jose <johny@playlyfe.com>

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
 var express = require('express');
-
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var bodyParser = require('body-parser');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.json({ ok: true });
+app.use(cookieParser());
+app.use(session({ resave: false, saveUninitialized: true, secret: 'foo' }));
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  if (req.session.requests == null) {
+    req.session.requests = 0;
+  } else {
+    req.session.requests++;
+  }
+  next();
 });
+
+app.get('/', function (req, res) {
+  res.json({ ok: true, requests: req.session.requests });
+});
+
+app.post('/', function (req, res) {
+  res.json(req.body);
+})
 
 app.listen(3000);
