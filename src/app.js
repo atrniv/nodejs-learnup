bcbda7c829a78ab1268b4dc98492e6297ecdd0ce
Johny Jose <johny@playlyfe.com>
2015-06-27 02:46:29 +0530

    feat(core): add html for the application
    Signed-off-by: Johny Jose <johny@playlyfe.com>

--- src/app.js
+++ src/app.js
@@ -14,7 +14,18 @@ app.use(bodyParser.json());
 app.use('/assets', express.static(__dirname + '/../bower_components'));
 
 app.get('/', function (req, res) {
-  res.render('home');
+  res.render('login');
 });
+
+app.get('/home', function (req, res) {
+  res.render('home', {
+    challenge: {
+      id: 1,
+      description: 'Print out the sequence of numbers from 1 to the given input.',
+      sample_input: '5',
+      sample_output: '1 2 3 4 5'
+    }
+  });
+})
 
 app.listen(3000);
